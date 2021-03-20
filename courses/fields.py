72587cc4b37e486f0d9b3a3be5d88065609fe93f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-20 19:42:53 -0300

    Included the OrderField in the Course Models and migrate new field

--- courses/fields.py
+++ courses/fields.py
@@ -16,15 +16,14 @@ class OrderField(models.PositiveIntegerField):
                     # filter by objects with the same field values
                     # for the fields in "for_fields"
                     query = {field:
-                             getattr(model_instance, field)
-                             for field in self.for_fields}
+                             getattr(model_instance, field) for field in self.for_fields}
                     qs = qs.filter(**query)
                 # get the order of the item
                 last_item = qs.latest(self.attname)
                 value = last_item.order + 1
             except ObjectDoesNotExist:
                 value = 0
-                setattr(model_instance, self.attname, value)
-                return value
+            setattr(model_instance, self.attname, value)
+            return value
         else:
             return super().pre_save(model_instance, add)
--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -39,9 +40,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -60,6 +65,10 @@ class Content(models.Model):
                                      })
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
