e59e93e73b2fcfbd040a29e5e6c4b133c1dc9074
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-21 22:15:53 -0300

    Implemented the template for login and logout

--- educa/urls.py
+++ educa/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('account/login/', auth_views.LoginView.as_view(), name='login'),
-    path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
