04d1d280755b91ec8de08cfe75d4a5c37eaba102
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-24 14:38:27 -0300

    Created class based view to update the courses modules using formset

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import (CreateView,
@@ -6,6 +9,34 @@ from django.views.generic.edit import (CreateView,
 from .models import Course
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+                             data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                       'formset': formset})
 
 
 class OwnerMixin(object):
