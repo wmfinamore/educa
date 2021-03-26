15dd3fec99d6122bbb8879a88dfc7e08d35f010b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-26 16:06:35 -0300

    Created class based view and your route to delete modules content

--- courses/urls.py
+++ courses/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
          views.ContentCreateUpdateView.as_view(), name='module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/',
          views.ContentCreateUpdateView.as_view(), name='module_content_update'),
+    path('content/<int:id>/delete/',
+         views.ContentDeleteView.as_view(), name='module_content_delete'),
 ]
