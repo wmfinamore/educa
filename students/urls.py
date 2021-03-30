d54a33fcbe55926e164d8b4bbad8ea19e47900f9
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 15:59:33 -0300

    Added route to enroll student

--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
+    path('enroll_course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
 ]
