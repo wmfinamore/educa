3857f4f452bcf89a61e333072c46cc950fa37956
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 17:02:02 -0300

    Added routes to list and detail courses in that the student was enrolled

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
-    path('enroll_course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('register/',
+         views.StudentRegistrationView.as_view(), name='student_registration'),
+    path('enroll_course/',
+         views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('courses/',
+         views.StudentCourseListView.as_view(), name='student_course_list'),
+    path('courses/<pk>/',
+         views.StudentCourseDetailView.as_view(), name='student_course_detail'),
+    path('courses/<pk>/<module_id>/',
+         views.StudentCourseDetailView.as_view(), name='student_course_detail_module'),
 ]
