021daa3039b9522a718c5c593c45382a26b5e0df
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 16:15:46 -0300

    Altered the get_context_data method in CourseDetail for include the CourseEnroll form inside of the context

--- courses/views.py
+++ courses/views.py
@@ -17,6 +17,7 @@ from .models import (Course,
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 from django.db.models import Count
+from students.forms import CourseEnrollForm
 
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
@@ -212,3 +213,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
