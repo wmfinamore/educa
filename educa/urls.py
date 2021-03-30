3dbd4e2dcb2f41e475212192d883fce0701c8059
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 09:50:39 -0300

    Added the route for Course List View in the main urls file

--- educa/urls.py
+++ educa/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
