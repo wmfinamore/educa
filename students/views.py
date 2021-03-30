8cb62c5bf32ee776009956b3d4d7f22166870ef0
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 16:56:33 -0300

    Implemented a class to detail a courses in that the student was enrolled

--- students/views.py
+++ students/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
 from django.views.generic.list import ListView
 from courses.models import Course
+from django.views.generic.detail import DetailView
 
 
 class StudentRegistrationView(CreateView):
@@ -45,3 +46,26 @@ class StudentCourseListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id']
+            )
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+        return context
