3e19258f3cd3bb7109b04a6eac2c804889b3f44d
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-30 09:58:34 -0300

    Added routes to list courses and your details filtered by subject

--- courses/urls.py
+++ courses/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
-    path('create/', views.CourseCreateView.as_view(), name='course_create'),
-    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
-    path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('mine/',
+         views.ManageCourseListView.as_view(), name='manage_course_list'),
+    path('create/',
+         views.CourseCreateView.as_view(), name='course_create'),
+    path('<pk>/edit/',
+         views.CourseUpdateView.as_view(), name='course_edit'),
+    path('<pk>/delete/',
+         views.CourseDeleteView.as_view(), name='course_delete'),
+    path('<pk>/module/',
+         views.CourseModuleUpdateView.as_view(), name='course_module_update'),
     path('module/<int:module_id>/content/<model_name>/create/',
          views.ContentCreateUpdateView.as_view(), name='module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/',
@@ -19,4 +24,8 @@ urlpatterns = [
          views.ModuleOrderView.as_view(), name='module_order'),
     path('content/order/',
          views.ContentOrderView.as_view(), name='content_order'),
+    path('subject/<slug:subject>/',
+         views.CourseListView.as_view(), name='course_list_subject'),
+    path('<slug:slug>/',
+         views.CourseListView.as_view(), name='course_detail'),
 ]
