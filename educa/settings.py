0392f8114a2b785a6e8f98fa457b466c4ea46298
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-31 23:00:34 -0300

    Implemented list and detail template to show coused when the student are enrolled

--- educa/settings.py
+++ educa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
