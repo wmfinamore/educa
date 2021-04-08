fe479b9e4e738067f61b330008af0a2d76c090e3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-04-07 21:57:41 -0300

    Fixed route to Channels version 3.x. Implemented javascript in chat room template to test websocket

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/room/(? P<course_id>\d+)/$', consumers.ChatConsumer),
+    # Included method .as_asgi() , similar to Django .as_view()
+    re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
